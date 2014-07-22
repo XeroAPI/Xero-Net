f4f49293091fefd94638ea1db97f4ab3840c6f87
Richard Rowley <richard.rowley@xero.com>
2014-07-22 12:37:07 +1200

    Add new test to ensure the allocation is on the credit note

--- CoreTests/Integration/Allocations/Add.cs
+++ CoreTests/Integration/Allocations/Add.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using CoreTests.Integration.Invoices;
 using NUnit.Framework;
 using Xero.Api.Core.Model;
@@ -14,7 +15,6 @@ namespace CoreTests.Integration.Allocations
         {
             var creditNote = new CreditNotes.CreditNotesTest().Given_an_authorised_creditnote(CreditNoteType.AccountsReceivable);
             var invoice = new Create().Given_an_authorised_invoice(InvoiceType.AccountsReceivable);
-
             var expected = Math.Min(creditNote.Total, invoice.Total.GetValueOrDefault());
 
             var result = Api.Allocations.Add(new Allocation
@@ -34,7 +34,6 @@ namespace CoreTests.Integration.Allocations
         {            
             var creditNote = new CreditNotes.CreditNotesTest().Given_an_authorised_creditnote();
             var invoice = new Create().Given_an_authorised_invoice();
-
             var expected = Math.Min(creditNote.Total, invoice.Total.GetValueOrDefault());
 
             var result = Api.Allocations.Add(new Allocation
@@ -47,6 +46,27 @@ namespace CoreTests.Integration.Allocations
             Assert.AreEqual(expected, result.Amount);
             Assert.AreEqual(invoice.Id, result.Invoice.Id);
             Assert.AreEqual(creditNote.Id, result.CreditNote.Id);
+        }
+
+
+        [Test]
+        public void allocation_on_creditnote()
+        {
+            var creditNote = new CreditNotes.CreditNotesTest().Given_an_authorised_creditnote();
+            var invoice = new Create().Given_an_authorised_invoice();
+            var expected = Math.Min(creditNote.Total, invoice.Total.GetValueOrDefault());
+
+            Api.Allocations.Add(new Allocation
+            {
+                AppliedAmount = expected,
+                CreditNote = new CreditNote { Id = creditNote.Id },
+                Invoice = new Invoice { Id = invoice.Id }
+            });
+
+            creditNote = Api.CreditNotes.Find(creditNote.Id);
+            
+            Assert.AreEqual(1, creditNote.Allocations.Count);
+            Assert.AreEqual(expected, creditNote.Allocations.First().Amount);
         }
     }
 }
