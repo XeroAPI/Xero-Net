f19e3656293a90db9f6a73e918b00b768706c455
Javed Mohammed <javed.mohammed@xero.com>
2015-02-11 13:46:15 +1300

    [WIP]

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Xero.Api.sln /t:Xero.Api /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Xero.Api.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
