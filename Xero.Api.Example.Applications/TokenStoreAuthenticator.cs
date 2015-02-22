2419d5c16ea7cce3a3f1c8e60e5078f41a86c3cf
Javed Mohammed <javed.mohammed@xero.com>
2015-02-23 12:29:21 +1300

    Support for posting callback url in oauth header

--- Xero.Api.Example.Applications/TokenStoreAuthenticator.cs
+++ Xero.Api.Example.Applications/TokenStoreAuthenticator.cs
@@ -89,20 +89,24 @@ namespace Xero.Api.Example.Applications
 
         protected IToken GetToken(IConsumer consumer)
         {
-            var oauthToken = Tokens.GetRequestToken(consumer, GetAuthorization(new Token
-            {
-                ConsumerKey = consumer.ConsumerKey,
-                ConsumerSecret = consumer.ConsumerSecret
-            }, "POST", Tokens.RequestUri));
+            var token = new Token
+                {
+                    ConsumerKey = consumer.ConsumerKey,
+                    ConsumerSecret = consumer.ConsumerSecret
+                };
 
-            var verifier = AuthorizeUser(oauthToken);
+            var requestTokenOAuthHeader = GetAuthorization(token, "POST", Tokens.RequestUri, callback: CallBackUri);
 
-            return Tokens.GetAccessToken(oauthToken,
-                GetAuthorization(oauthToken, "POST", Tokens.AccessUri, null, verifier));
+            var requestToken = Tokens.GetRequestToken(consumer, requestTokenOAuthHeader);
+
+            var verifier = AuthorizeUser(requestToken);
+
+            return Tokens.GetAccessToken(requestToken,
+                GetAuthorization(requestToken, "POST", Tokens.AccessUri, null, verifier));
         }
 
-        protected string GetAuthorization(IToken token, string verb, string endpoint, string query = null, 
-            string verifier = null, bool renewToken = false)
+        protected string GetAuthorization(IToken token, string verb, string endpoint, string query = null,
+            string verifier = null, bool renewToken = false, string callback = null)
         {
             var uri = new UriBuilder(BaseUri)
             {
@@ -114,7 +118,7 @@ namespace Xero.Api.Example.Applications
                 uri.Query = query.TrimStart('?');
             }
 
-            return CreateSignature(token, verb, uri.Uri, verifier, renewToken);
+            return CreateSignature(token, verb, uri.Uri, verifier, renewToken, callback);
         }
     }
 }
