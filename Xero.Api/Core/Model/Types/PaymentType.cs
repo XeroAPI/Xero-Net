12c10949acb2d7fae3524e91ba35202911d938f6
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-04-02 09:21:07 +1300

    Added payment types for prepayments and overpayments

--- Xero.Api/Core/Model/Types/PaymentType.cs
+++ Xero.Api/Core/Model/Types/PaymentType.cs
@@ -7,10 +7,18 @@ namespace Xero.Api.Core.Model.Types
         [EnumMember(Value = "ACCPAYPAYMENT")]
         AccountsPayable,
         [EnumMember(Value = "ACCRECPAYMENT")]
-        AccountsReceivable
+        AccountsReceivable,
         [EnumMember(Value = "APCREDITPAYMENT")]
         AccountsPayableCredit,
         [EnumMember(Value = "ARCREDITPAYMENT")]
         AccountsReceivableCredit,
+        [EnumMember(Value = "ARPREPAYMENTPAYMENT")]
+        AccountsReceivablePrepayment,
+        [EnumMember(Value = "APPREPAYMENTPAYMENT")]
+        AccountsPayablePrepayment,
+        [EnumMember(Value = "AROVERPAYMENTPAYMENT")]
+        AccountsReceivableOverpayment,
+        [EnumMember(Value = "APOVERPAYMENTPAYMENT")]
+        AccountsPayableOverpayment,
     }
 }
