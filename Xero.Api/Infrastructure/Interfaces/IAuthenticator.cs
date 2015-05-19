935fb8f07e800420a076694eb0c36e89d3aabf79
Ben Biddington <ben.biddington@xero.com>
2015-05-19 15:38:07 +1200

    Deleted unused members

--- Xero.Api/Infrastructure/Interfaces/IAuthenticator.cs
+++ Xero.Api/Infrastructure/Interfaces/IAuthenticator.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 
 namespace Xero.Api.Infrastructure.Interfaces
 {
     public interface IAuthenticator
     {
-        [Obsolete("Use Authenticate instead")]
-        string GetSignature(IConsumer consumer, IUser user, Uri uri, string verb, IConsumer consumer1);
-
-        [Obsolete("Use Authenticate instead")]
-        IToken GetToken(IConsumer consumer, IUser user);
-
-        [Obsolete("Use Authenticate instead")]
-        IUser User { get; set; }
-
         void Authenticate(HttpWebRequest request);
     }
 }
