df65405e8b09b36789a6c841c1a63dc53c7a9d1f
Ben Biddington <ben.biddington@xero.com>
2015-05-19 16:21:30 +1200

    Refactor: extracted function <Binary#Write>

---
+++ Xero.Api/Private/BlankAuthenticator.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Xero.Api.Infrastructure.Interfaces;
+
+namespace Xero.Api.Private
+{
+    internal class BlankAuthenticator : IAuthenticator
+    {
+        public void Authenticate(HttpWebRequest request)
+        { }
+    }
+}
