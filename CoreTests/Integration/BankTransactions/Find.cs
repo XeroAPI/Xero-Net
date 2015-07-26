dda52dd3a87c8b8d4e874976b16ec4e600d41c7e
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-07-27 11:41:41 +1200

    Add paging to bank transactions. The endpoint will not page by default yet aqs this would be a breaking change

--- CoreTests/Integration/BankTransactions/Find.cs
+++ CoreTests/Integration/BankTransactions/Find.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace CoreTests.Integration.BankTransactions
@@ -35,6 +36,15 @@ namespace CoreTests.Integration.BankTransactions
             var id = Api.BankTransactions.Find(expected).Id;
 
             Assert.AreEqual(expected, id);
+        }
+
+        [Test]
+        public void find_by_page()
+        {
+            Given_a_bank_transaction();
+            var bankTrans = Api.BankTransactions.Page(1).Find();
+
+            Assert.Greater(bankTrans.Count(), 0);
         }
     }
 }
