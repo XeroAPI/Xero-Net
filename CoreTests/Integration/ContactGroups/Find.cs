386a3057e277ed84c906f19d974dd0763257b2b4
Phil Alsford <phil.alsford@xero.com>
2015-08-20 13:37:45 +1200

    No functional changes
    Adds example test around ContactGroups.Find()

---
+++ CoreTests/Integration/ContactGroups/Find.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CoreTests.Integration.ContactGroups
+{
+    [TestFixture]
+    public class Find : ContactGroupsTest
+    {
+        [Test]
+        public void Can_Find_Contact_Group()
+        {
+            var contactGroup = Given_a_contactgroup();
+
+            var foundContactGroup = Api.ContactGroups.Find(contactGroup.Id);
+
+            Assert.IsTrue(foundContactGroup.Name.StartsWith("Nice People"));
+        }
+
+        [Test]
+        public void Can_Find_Contacts_in_Contact_Group()
+        {
+            var contactGroup = Given_a_contactgroup();
+
+            var contact = Given_a_contact();
+
+            Api.ContactGroups[contactGroup.Id].Add(contact);
+
+            var foundContactGroup = Api.ContactGroups.Find(contactGroup.Id);
+
+            Assert.IsTrue(foundContactGroup.Name.StartsWith("Nice People"));
+            Assert.IsTrue(foundContactGroup.Contacts.FirstOrDefault().Name.StartsWith("Peter"));
+        }
+    }
+}
