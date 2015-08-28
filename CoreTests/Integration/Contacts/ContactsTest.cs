7bb7d7ee141e04c2ca3855e88327614bf625b6b2
Phil Alsford <phil.alsford@xero.com>
2015-08-28 14:31:41 +1200

    Contact has a class called DefaultTrackingCategory which would serialize out to that and not PurchasesTrackingCategories/SalesTrackingCategories.
    This commit replaces that with two classes that will. I have left them in the same file to make them easier to find.
    
    Adds a test which adds a Contact with PurchasesTrackingCategories. It creates the TC if it is not there. It deletes it after.

--- CoreTests/Integration/Contacts/ContactsTest.cs
+++ CoreTests/Integration/Contacts/ContactsTest.cs
@@ -1,9 +1,15 @@
-﻿using Xero.Api.Core.Model;
+﻿using System;
+using System.Linq;
+using Xero.Api.Core.Model;
+using Xero.Api.Core.Model.Status;
 
 namespace CoreTests.Integration.Contacts
 {
     public abstract  class ContactsTest : ApiWrapperTest
     {
+        private TrackingCategory trackingCat;
+        private bool wasTCCreated = false;
+
         protected Contact Given_a_contact()
         {
             var contact = Api.Create(new Contact
@@ -11,6 +17,43 @@ namespace CoreTests.Integration.Contacts
                 Name = "Peter " + Random.GetRandomString(10)
             });
             return contact;
+        }
+
+        protected TrackingCategory findOrCreateTC(string OptionName, string TCName)
+        {
+            trackingCat = Api.TrackingCategories.GetAll().FirstOrDefault();
+            if (trackingCat == null || trackingCat.Options.FirstOrDefault() == null)
+            {
+                var option1 = new Option()
+                {
+                    Id = Guid.Empty,
+                    Name = OptionName,
+                    Status = TrackingOptionStatus.Active
+                };
+
+
+                trackingCat = Api.TrackingCategories.Create(new TrackingCategory()
+                {
+                    Name = TCName,
+                    Status = TrackingCategoryStatus.Active
+                });
+
+
+                Api.TrackingCategories[trackingCat.Id].Add(option1);
+
+                trackingCat = Api.TrackingCategories.GetByID(trackingCat.Id);
+                wasTCCreated = true;
+            }
+            return trackingCat;
+        }
+
+        protected void deleteCreatedTC(TrackingCategory tc)
+        {
+            if (wasTCCreated)
+            {
+                Api.TrackingCategories.Delete(trackingCat);
+                wasTCCreated = false;
+            }
         }
     }
 }
--- Xero.Api/Core/Model/ContactTrackingCategory.cs
+++ Xero.Api/Core/Model/ContactTrackingCategory.cs
@@ -4,7 +4,17 @@ using System.Runtime.Serialization;
 namespace Xero.Api.Core.Model
 {
     [DataContract(Namespace = "")]
-    public class DefaultTrackingCategory
+    public class SalesTrackingCategory
+    {
+        [DataMember(Name = "TrackingCategoryName", EmitDefaultValue = false)]
+        public String Name { get; set; }
+
+        [DataMember(Name = "TrackingOptionName", EmitDefaultValue = false)]
+        public String Option { get; set; }
+    }
+
+    [DataContract(Namespace = "")]
+    public class PurchasesTrackingCategory
     {
         [DataMember(Name = "TrackingCategoryName", EmitDefaultValue = false)]
         public String Name { get; set; }
