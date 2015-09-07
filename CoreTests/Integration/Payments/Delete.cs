bab515f0254488535c7f61e4e115301e915cb789
Phil Alsford <phil.alsford@xero.com>
2015-09-08 08:39:43 +1200

    NO FUNCTIONAL CHANGE Adds a test showing how to delete a payment

---
+++ CoreTests/Integration/Payments/Delete.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using Xero.Api.Core.Model;
+using Xero.Api.Core.Model.Status;
+
+namespace CoreTests.Integration.Payments
+{
+    [TestFixture]
+    public class Delete : PaymentsTest
+    {
+        [TestFixtureSetUp]
+        public void CreatePaymentsSetUp()
+        {
+            SetUp();
+        }
+
+        [Test]
+        public void can_delete_payments()
+        {
+            var date = DateTime.UtcNow;
+            const decimal expected = 32.6m;
+            const decimal invoiceAmount = 100;
+
+            var payment = Given_a_payment(invoiceAmount, date, expected);
+
+            Given_this_payment_is_deleted(payment);
+
+            var found = Api.Payments.Find(payment.Id);
+
+            Assert.True(found.Status == PaymentStatus.Deleted);
+        }
+
+    }
+}
