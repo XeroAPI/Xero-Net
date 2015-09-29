fc82b8f671e533385307534b5a1fc5f2071194f4
Matthew Steeples <matthew@mercuryit.co.uk>
2015-09-30 00:26:09 +0100

    Add unit test for RateLimiter

---
+++ CoreTests/Unit/RateLimiterTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Xero.Api.Infrastructure.RateLimiter;
+
+namespace CoreTests.Unit
+{
+    [TestFixture]
+    public class RateLimiterTest
+    {
+        [Test]
+        public void TestRateLimiter()
+        {
+            RateLimiter rate = new RateLimiter(TimeSpan.FromSeconds(5), 5);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Assert.IsFalse(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            Assert.IsFalse(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            Assert.IsFalse(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            Assert.IsFalse(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            Assert.IsFalse(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            Assert.IsTrue(rate.CheckLimit());
+            rate.WaitUntilLimit();
+            sw.Stop();
+            Assert.IsTrue(sw.Elapsed > TimeSpan.FromSeconds(5));
+        }
+    }
+}
