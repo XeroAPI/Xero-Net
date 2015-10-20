47018577daa4be3c256ec9f817545da8ee8889e0
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-10-20 19:03:56 +1300

    Revert program file back to master

--- Xero.Api.Example.Counts/Program.cs
+++ Xero.Api.Example.Counts/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using Xero.Api.Common;
-using Xero.Api.Core.Model;
-using Xero.Api.Core.Model.Status;
-using Xero.Api.Core.Model.Types;
+using Xero.Api.Core;
 using Xero.Api.Example.TokenStores;
 using Xero.Api.Infrastructure.OAuth;
 
@@ -14,72 +10,14 @@ namespace Xero.Api.Example.Counts
         static void Main(string[] args)
         {
             var user = new ApiUser { Name = Environment.MachineName };
-            var tokenStore = new MemoryTokenStore();
+            var tokenStore = new SqliteTokenStore();
 
-            var api = new Applications.Private.Core()
+            var api = new Applications.Public.Core(tokenStore, user)
             {
                 UserAgent = "Xero Api - Listing example"
             };
 
-            var cN = api.CreditNotes.Update(new CreditNote
-            {
-                Contact = new Contact { Name = "abc" },
-                Number = "cnhelpsd",
-                Type = CreditNoteType.AccountsPayable,
-                Date = DateTime.UtcNow,
-                LineAmountTypes = LineAmountType.Exclusive,
-                Status = InvoiceStatus.Draft,
-                LineItems = new List<LineItem>
-                {
-                    new LineItem
-                    {
-                        AccountCode = "720",
-                        Description = "MacBook - White",
-                        UnitAmount = 1995.00m
-                    }
-                }
-            });
-
-            //var inv = api.Invoices.Create(new Invoice
-            //{
-            //    Contact = new Contact { Name = "ABC Bank" },
-            //    Type = InvoiceType.AccountsPayable,
-            //    Date = DateTime.UtcNow,
-            //    DueDate = DateTime.UtcNow.AddDays(90),
-            //    LineAmountTypes = LineAmountType.Inclusive,
-            //    Status = InvoiceStatus.Draft,
-            //    LineItems = new List<LineItem>
-            //    {
-            //        new LineItem
-            //        {
-            //            AccountCode = "200",
-            //            Description = "Good value item",
-            //            LineAmount = 100m
-            //        },
-            //        new LineItem
-            //        {
-            //            AccountCode = "200",
-            //            Description = "Good value item",
-            //            LineAmount = 100m
-            //        }
-            //    }
-
-            //});
-
-
-            //var lt = new LinkedTransaction
-            //{
-            //    SourceTransactionID = inv.Id,
-            //    SourceLineItemID = inv.LineItems[0].LineItemId
-            //};
-
-            //var x = api.LinkedTransactions.Create(lt);
-
-            //x.SourceLineItemID = inv.LineItems[1].LineItemId;
-
-            //var updated = api.LinkedTransactions.Update(x);
-
-            //var get = api.LinkedTransactions.Find(x.Id);
+            new Lister(api).List();            
         }
     }
 }
