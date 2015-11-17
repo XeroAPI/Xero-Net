4213a5aa7114758f7681c2bd55755462588ec73d
Phil Alsford <phil.alsford@xero.com>
2015-11-18 11:52:03 +1300

    Adds DELETE to the Inventory Items endpoint.
    Also adds examples.

---
+++ CoreTests/Integration/Items/Delete.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CoreTests.Integration.Items.TrackedItems;
+using NUnit.Framework;
+using Xero.Api.Core.Model;
+using Xero.Api.Infrastructure.Exceptions;
+
+namespace CoreTests.Integration.Items
+{
+    [TestFixture]
+    public class Delete : TrackedInventoryTest
+    {
+        [Test]
+        public void you_can_delete_an_item()
+        {
+            Given_a_tracked_item();
+
+            Api.Items.Delete(CreatedItem);
+
+            Its_not_there_when_I_try_to_find_it(CreatedItem);
+        }
+
+        [Test]
+        public void but_not_if_its_involved_in_a_transaction()
+        {
+            Given_a_tracked_item();
+
+            Given_an_ACCPAY_invoice_using_the_item_with_code(CreatedItem.Code);
+
+            When_I_try_to_Delete_it_fails();
+        }
+
+        [Test]
+        public void and_it_will_404_if_you_try_to_delete_a_item_thats_not_there()
+        {
+            CreatedItem = new Item
+            {
+                Id = Guid.NewGuid()
+            };
+
+            When_I_try_to_Delete_it_404s();
+        }
+
+
+        private void When_I_try_to_Delete_it_404s()
+        {
+            Assert.Throws<NotFoundException>(() => Api.Items.Delete(CreatedItem));
+        }
+
+        private void When_I_try_to_Delete_it_fails()
+        {
+            Assert.Throws<ValidationException>(() => Api.Items.Delete(CreatedItem));
+        }
+
+        private void Its_not_there_when_I_try_to_find_it(Item theItem)
+        {
+            Assert.Throws<NotFoundException>(() => Api.Items.Find(theItem.Id));
+        }
+    }
+}
--- Xero.Api/Core/Endpoints/ItemsEndpoint.cs
+++ Xero.Api/Core/Endpoints/ItemsEndpoint.cs
@@ -1,4 +1,6 @@
-﻿using Xero.Api.Core.Endpoints.Base;
+﻿using System.Linq;
+using System.Net;
+using Xero.Api.Core.Endpoints.Base;
 using Xero.Api.Core.Model;
 using Xero.Api.Core.Request;
 using Xero.Api.Core.Response;
@@ -12,6 +14,28 @@ namespace Xero.Api.Core.Endpoints
         public ItemsEndpoint(XeroHttpClient client) :
             base(client, "/api.xro/2.0/Items")
         {
+        }
+
+        public void Delete(Item itemToDelete)
+        {
+            var endpoint = string.Format("/api.xro/2.0/Items/{0}", itemToDelete.Id);
+
+            HandleResponse(Client
+                .Client
+                .Delete(endpoint));
+        }
+
+        private ItemsResponse HandleResponse(Infrastructure.Http.Response response)
+        {
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = Client.JsonMapper.From<ItemsResponse>(response.Body);
+                return result;
+            }
+
+            Client.HandleErrors(response);
+
+            return null;
         }
     }
 }
