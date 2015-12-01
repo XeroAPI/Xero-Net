c2dc1a639c6006d2d66fc5b8fe5c9bca0cae79d5
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-12-02 12:02:54 +1300

    Add signing cert password functionality to other PartnerAuthenticators

--- Xero.Api.Example.Applications/Partner/PartnerAuthenticator.cs
+++ Xero.Api.Example.Applications/Partner/PartnerAuthenticator.cs
@@ -16,11 +16,11 @@ namespace Xero.Api.Example.Applications.Partner
         {            
         }
 
-        public PartnerAuthenticator(string baseUri, string authorizeUri, string callBackUri, ITokenStore store, string signingCertificatePath, string certificatePath, string password)
+        public PartnerAuthenticator(string baseUri, string authorizeUri, string callBackUri, ITokenStore store, string signingCertificatePath, string certificatePath, string entrustPassword, string signingCertPassword = "")
             : this(baseUri, authorizeUri, callBackUri, store)
         {
-            _signingCertificate = new X509Certificate2(signingCertificatePath);
-            _certificate = new X509Certificate2(certificatePath, password);            
+            _signingCertificate = new X509Certificate2(signingCertificatePath, signingCertPassword, X509KeyStorageFlags.MachineKeySet);
+            _certificate = new X509Certificate2(certificatePath, entrustPassword);            
         }
 
         public PartnerAuthenticator(string baseUri, string authorizeUri, string callBackUri, ITokenStore store, X509Certificate2 signingCertificate, X509Certificate2 certificate)
