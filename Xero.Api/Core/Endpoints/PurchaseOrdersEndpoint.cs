2569cbe5c79afc9ea2d4d80e5369d86a3f03e292
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-12-10 13:07:31 +1300

    Add PUT PurchaseOrder functionality

--- Xero.Api/Core/Endpoints/PurchaseOrdersEndpoint.cs
+++ Xero.Api/Core/Endpoints/PurchaseOrdersEndpoint.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using Xero.Api.Common;
+using Xero.Api.Core.Endpoints.Base;
 using Xero.Api.Core.Model;
 using Xero.Api.Core.Model.Status;
+using Xero.Api.Core.Request;
 using Xero.Api.Core.Response;
 using Xero.Api.Infrastructure.Http;
 
 namespace Xero.Api.Core.Endpoints
 {
-    public class PurchaseOrdersEndpoint : XeroReadEndpoint<PurchaseOrdersEndpoint, PurchaseOrder, PurchaseOrdersResponse>
+    public class PurchaseOrdersEndpoint : XeroCreateEndpoint<PurchaseOrdersEndpoint, PurchaseOrder, PurchaseOrdersRequest, PurchaseOrdersResponse>
     {
         public PurchaseOrdersEndpoint(XeroHttpClient client) :
             base(client, "/api.xro/2.0/PurchaseOrders")
