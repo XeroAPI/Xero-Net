dbf978b6356f93e968e80a04fac0fd40f55b9ee8
Matthew Mortimer <matthew.mortimer1@gmail.com>
2015-12-10 15:37:22 +1300

    Add create examples for PUT purchase orders

---
+++ CoreTests/Integration/PurchaseOrders/Create.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Xero.Api.Core.Model;
+using Xero.Api.Core.Model.Status;
+
+namespace CoreTests.Integration.PurchaseOrders
+{
+    public class Create : ApiWrapperTest
+    {
+        [Test]
+        public void Create_minimal_draft_purchase_order()
+        {
+
+            var purchaseOrder = Api.PurchaseOrders.Create(
+                new PurchaseOrder
+                {
+                    Date = DateTime.Today,
+                    Contact = new Contact { Id = ContactId }
+                }
+            );
+
+            Assert.True(purchaseOrder.Id != Guid.Empty);
+            Assert.True(purchaseOrder.Status == PurchaseOrderStatus.Draft);
+        }
+
+        [Test]
+        public void Create_authorised_purchase_order()
+        {
+            var purchaseOrder = Api.PurchaseOrders.Create(
+                new PurchaseOrder
+                {
+                    Status = PurchaseOrderStatus.Authorised,
+                    Date = DateTime.Today,
+                    Contact = new Contact{Id = ContactId},
+                    LineItems = new List<LineItem>()
+                    {
+                        new LineItem
+                        {
+                            Description = "An item I want to purchase",
+                            UnitAmount = 1,
+                            Quantity = 1,
+
+                        }
+                    }
+                }
+            );
+
+            Assert.True(purchaseOrder.Id != Guid.Empty);
+            Assert.True(purchaseOrder.Status == PurchaseOrderStatus.Authorised);
+        }
+
+        private Guid ContactId
+        {
+            get
+            {
+                return Api.Contacts.Find().First().Id;
+            }
+        }
+    }
+}
