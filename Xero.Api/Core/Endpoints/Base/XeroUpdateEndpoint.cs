48e22f2c27654e3cbd0e144033d393013644166f
Matthew Steeples <matthew@mercuryit.co.uk>
2015-12-15 21:30:04 +0000

    Add interface for update endpoints

--- Xero.Api/Core/Endpoints/Base/XeroUpdateEndpoint.cs
+++ Xero.Api/Core/Endpoints/Base/XeroUpdateEndpoint.cs
@@ -7,7 +7,7 @@ using Xero.Api.Infrastructure.Interfaces;
 namespace Xero.Api.Core.Endpoints.Base
 {
     public abstract class XeroUpdateEndpoint<T, TResult, TRequest, TResponse>
-        : XeroCreateEndpoint<T, TResult, TRequest, TResponse>
+        : XeroCreateEndpoint<T, TResult, TRequest, TResponse>, IXeroUpdateEndpoint<T, TResult, TRequest, TResponse>
         where T : XeroReadEndpoint<T, TResult, TResponse>
         where TResponse : IXeroResponse<TResult>, new()
         where TRequest : IXeroRequest<TResult>, new()
