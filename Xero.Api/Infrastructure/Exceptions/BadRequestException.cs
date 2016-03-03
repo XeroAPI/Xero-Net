a935bd2d799552c168503ce71a0f8319c80b0fc3
Matthew Steeples <matthew@mercuryit.co.uk>
2016-03-03 13:41:01 +0000

    Reveal details of successful elements even when some failed

--- Xero.Api/Infrastructure/Exceptions/BadRequestException.cs
+++ Xero.Api/Infrastructure/Exceptions/BadRequestException.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using Xero.Api.Infrastructure.Model;
 
 namespace Xero.Api.Infrastructure.Exceptions
@@ -10,10 +11,12 @@ namespace Xero.Api.Infrastructure.Exceptions
             : base(HttpStatusCode.BadRequest, apiException.Message)
         {
             ErrorNumber = apiException.ErrorNumber;
-            Type = apiException.Type;            
+            Type = apiException.Type;
+            Elements = apiException.Elements;     
         }
 
         public int ErrorNumber { get; private set; }
-        public string Type { get; private set; }        
+        public string Type { get; private set; }
+        public ICollection<DataContractBase> Elements { get; private set; }
     }
 }
