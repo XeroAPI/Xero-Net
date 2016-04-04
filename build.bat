12df6fb249834f193daca2dc7abcc197a94b7f70
Phil Alsford <phil.alsford@xero.com>
2016-04-05 10:08:32 +1200

    Stop Nuget packages being restored by msbuild http://www.xavierdecoster.com/migrate-away-from-msbuild-based-nuget-package-restore

--- build.bat
+++ build.bat
@@ -14,8 +14,16 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Xero.Api.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+rem About to restore packages
+call %NuGet% restore
+
+rem About to build solution
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Xero.Api.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
+
+rem About to pack Nugets
+call %NuGet% pack "minimal.nuspec" -Symbols -OutputDirectory Build\lib\net40 -Version %version%
+call %NuGet% pack "nuget.nuspec" -symbols -OutputDirectory  Build\lib\net40 -Version %version%
