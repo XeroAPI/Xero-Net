634060e6fcba22a27419cc556ef935a93c78a086
Ben Biddington <ben.biddington@xero.com>
2016-04-28 15:14:58 +1200

    Expand examples

--- CoreTests/Unit/BaseUrlConfiguration/PayrollUrlConfiguration.cs
+++ CoreTests/Unit/BaseUrlConfiguration/PayrollUrlConfiguration.cs
@@ -8,6 +8,8 @@ namespace CoreTests.Unit.BaseUrlConfiguration
     [TestFixture]
     public class PayrollUrlConfiguration
     {
+        // [i] SuperFundsEndpoint just chosen as an example -- subclass of `PayrollEndpoint` will do
+
         [Test]
         public void each_endpoint_instructs_its_client_to_ignore_path_segments_on_is_base_url()
         {
@@ -16,6 +18,26 @@ namespace CoreTests.Unit.BaseUrlConfiguration
             new SuperFundsEndpoint(xeroHttpClient);
 
             Assert.AreEqual("http://api.xero.com", xeroHttpClient.BaseUri, "Expected the <XeroHttpClient> to be configured with a new base url");
+        }
+
+        [Test]
+        public void it_can_be_anything()
+        {
+            var xeroHttpClient = new XeroHttpClient("http://any.host.name/xxx/yyy/zzz/", new BlankAuthenticator(), null, null, null, null);
+
+            new SuperFundsEndpoint(xeroHttpClient);
+
+            Assert.AreEqual("http://any.host.name", xeroHttpClient.BaseUri, "Expected the <XeroHttpClient> to be configured with a new base url");
+        }
+
+        [Test]
+        public void even_a_slash()
+        {
+            var xeroHttpClient = new XeroHttpClient("http://any.host.name/", new BlankAuthenticator(), null, null, null, null);
+
+            new SuperFundsEndpoint(xeroHttpClient);
+
+            Assert.AreEqual("http://any.host.name", xeroHttpClient.BaseUri, "Expected the <XeroHttpClient> to be configured with a new base url");
         }
     }
 }
