e9da886db7b60332ff7e4c80000b4616f48e1c40
Matthew Steeples <matthew@mercuryit.co.uk>
2016-05-17 19:13:07 +0100

    Add parameterless constructor to exceptions

--- Xero.Api/Infrastructure/Exceptions/NotFoundException.cs
+++ Xero.Api/Infrastructure/Exceptions/NotFoundException.cs
@@ -1,10 +1,14 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Xero.Api.Infrastructure.Exceptions
 {
+    [Serializable]
     public class NotFoundException
         : XeroApiException
     {
+        public NotFoundException() { }
+
         public NotFoundException(string body)
             : base(HttpStatusCode.NotFound, body)
         {
