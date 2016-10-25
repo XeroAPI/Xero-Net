d6f691b3bf778975ad842096f5d196d9353e0623
Wes Jones <wessjones@gmail.com>
2016-10-25 13:41:54 +1100

    Async rate limiter implementation

--- Xero.Api/Infrastructure/RateLimiter/RateLimiter.async.cs
+++ Xero.Api/Infrastructure/RateLimiter/RateLimiter.async.cs
@@ -9,9 +9,26 @@ namespace Xero.Api.Infrastructure.RateLimiter
 {
     partial class RateLimiter : IAsyncRateLimiter
     {
-        public Task WaitUntilLimitAsync(CancellationToken cancellation)
+        static TimeSpan timerPeriodInfinite = TimeSpan.FromMilliseconds(-1);
+
+        public async Task WaitUntilLimitAsync(CancellationToken cancellation)
         {
-            // TODO: Proper rate limiting check
+            while (rateLimiter.Count >= _qty)
+            {
+                var diff = rateLimiter.Peek().Add(_duration) - DateTime.UtcNow;
+                if (diff.TotalMilliseconds > 0)
+                {
+                    await this.SleepAsync(diff.Add(TimeSpan.FromSeconds(1)), cancellation);
+                }
+
+                rateLimiter.Dequeue();
+            }
+
+            rateLimiter.Enqueue(DateTime.UtcNow);
+        }
+
+        private Task SleepAsync(TimeSpan duration, CancellationToken cancellation)
+        {
             var completion = new TaskCompletionSource<bool>();
             var timer = new Timer(
                 c =>
@@ -19,8 +36,8 @@ namespace Xero.Api.Infrastructure.RateLimiter
                     ((TaskCompletionSource<bool>)c).SetResult(true);
                 },
                 completion,
-                1000,
-                0);
+                duration,
+                timerPeriodInfinite);
 
             cancellation.Register(timer.Dispose);
             return completion.Task;
