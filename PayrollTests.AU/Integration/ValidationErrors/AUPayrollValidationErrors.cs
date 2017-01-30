129bdff87873e376a04ba9789e5fa7026453ea26
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-01-30 13:48:01 +1300

    Added test for payroll validation exceptions

---
+++ PayrollTests.AU/Integration/ValidationErrors/AUPayrollValidationErrors.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Xero.Api.Infrastructure.Exceptions;
+using Xero.Api.Payroll.Australia.Model;
+
+namespace PayrollTests.AU.Integration.ValidationErrors
+{
+    public class AUPayrollValidationErrors : ApiWrapperTest
+    {
+        [Test]
+        public void Validation_errors_are_returned_from_the_AU_payroll_api()
+        {
+            var employee = new Employee() {FirstName = "Jimmy"};
+
+            try
+            {
+                Api.Employees.Create(employee);
+            }
+            catch (ValidationException e)
+            {
+                Assert.True(e.ValidationErrors.Any(p => p.Message == "The Last Name is required."));
+                Assert.True(e.ValidationErrors.Any(p => p.Message == "The Home Address is required."));
+                return;
+            }
+
+            throw new AssertionException("Expected validation errors");
+        }
+    }
+}
