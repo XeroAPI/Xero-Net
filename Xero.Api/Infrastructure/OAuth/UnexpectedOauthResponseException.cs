407039c19f807255f0faf642bc05d6b3b32432f0
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-30 11:47:49 +1300

    Throw different exception if OAuth operations fail for non oauth_problem reasons

---
+++ Xero.Api/Infrastructure/OAuth/UnexpectedOauthResponseException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Xero.Api.Infrastructure.OAuth
+{
+    public class UnexpectedOauthResponseException : Exception
+    {
+        public HttpStatusCode ResponseStatusCode { get; set; }
+        public string ResponseBody { get; set; }
+
+        public UnexpectedOauthResponseException(HttpStatusCode responseStatusCode, string responseBody) : base("An unexpected HTTP response was returned while performing OAuth operations")
+        {
+            ResponseStatusCode = responseStatusCode;
+            ResponseBody = responseBody;
+        }
+    }
+}
