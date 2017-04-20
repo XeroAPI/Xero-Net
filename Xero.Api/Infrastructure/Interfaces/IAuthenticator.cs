bef256d6650bd2c74778253d84408c871da8379d
Andrew Barrett <andrew.barrett@arlo.co>
2017-04-20 15:32:20 +1200

    Forgot to commit IAuthenticator

--- Xero.Api/Infrastructure/Interfaces/IAuthenticator.cs
+++ Xero.Api/Infrastructure/Interfaces/IAuthenticator.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Xero.Api.Infrastructure.Interfaces
 {
     public interface IAuthenticator
     {
-        string GetSignature(IConsumer consumer, IUser user, Uri uri, string verb, IConsumer consumer1);
+        string GetSignature(IConsumer consumer, IUser user, Uri uri, string verb, IConsumer consumer1, IEnumerable<KeyValuePair<string, string>> additionalParameters = null);
         IToken GetToken(IConsumer consumer, IUser user);
 
         IUser User { get; set; }
