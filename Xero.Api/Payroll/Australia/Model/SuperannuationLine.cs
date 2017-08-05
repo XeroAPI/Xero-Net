b97d7f0993702441cb1a3ed1318c8f7a5a53b15c
rohitsies <rohitsies@gmail.com>
2017-08-05 23:37:59 +1000

    If ContributionType =SuperannuationGuaranteeCharge and "find payslips" api is used it retrieves ContributionType=null, works fine for other types. making it non nullable fixes it. api.Payslips.Find(payslipId);

--- Xero.Api/Payroll/Australia/Model/SuperannuationLine.cs
+++ Xero.Api/Payroll/Australia/Model/SuperannuationLine.cs
@@ -11,7 +11,7 @@ namespace Xero.Api.Payroll.Australia.Model
         public Guid SuperMembershipId { get; set; }
 
         [DataMember]
-        public SuperannuationContributionType? ContributionType { get; set; }
+        public SuperannuationContributionType ContributionType { get; set; }
 
         [DataMember]
         public SuperannuationCalculationType? CalculationType { get; set; }
