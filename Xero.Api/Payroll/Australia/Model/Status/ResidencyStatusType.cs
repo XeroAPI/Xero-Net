5159705a53c2681cc45e991535c08d38f9969e26
Shaun Saward <shaunsaward@gmail.com>
2017-12-02 16:31:46 +1100

    * added ResidencyStatus to TaxDeclaration

---
+++ Xero.Api/Payroll/Australia/Model/Status/ResidencyStatusType.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Xero.Api.Payroll.Australia.Model.Status
+{
+    [DataContract(Namespace = "")]
+    public enum ResidencyStatus
+    {
+        [EnumMember(Value = "AUSTRALIANRESIDENT")]
+        AustralianResident,
+        [EnumMember(Value = "FOREIGNRESIDENT")]
+        ForeignResident,
+        [EnumMember(Value = "WORKINGHOLIDAYMAKER")]
+        WorkingHoliday,
+    }
+}
