e0fa0cb5befcd1d53405891ef9b4f4cfcf634b1e
Matthew Steeples <matthew@ledgerscope.com>
2018-03-20 07:48:29 +0000

    Add unit tests to cover serialization and deserialization

---
+++ CoreTests/Integration/CreditNotes/Deserialization.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xero.Api.Core.Model;
+using Xero.Api.Infrastructure.Interfaces;
+using Xero.Api.Serialization;
+using Xero.Api.Core.Model.Types;
+
+namespace CoreTests.Integration.CreditNotes
+{
+    [TestFixture]
+    public class Deserialization
+    {
+        private IJsonObjectMapper _jsonMapper;
+        private IXmlObjectMapper _xmlMapper;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _jsonMapper = new DefaultMapper();
+            _xmlMapper = new DefaultMapper();
+        }
+
+        [Test]
+        public void nullable_type_roundtrip_json()
+        {
+            var creditNote = createCreditNote();
+
+            var json = _jsonMapper.To(creditNote);
+
+            var deserialized = _jsonMapper.From<CreditNote>(json);
+
+            Assert.AreEqual(creditNote.Id, deserialized.Id);
+            Assert.AreEqual(creditNote.Type, deserialized.Type);
+        }
+
+        [Test]
+        public void nullable_type_roundtrip_xml()
+        {
+            var creditNote = createCreditNote();
+
+            var xml = _xmlMapper.To(creditNote);
+
+            var deserialized = _xmlMapper.From<CreditNote>(xml);
+
+            Assert.AreEqual(creditNote.Id, deserialized.Id);
+            Assert.AreEqual(creditNote.Type, deserialized.Type);
+        }
+
+        private static CreditNote createCreditNote()
+        {
+            return new CreditNote()
+            {
+                Id = Guid.NewGuid(),
+                Type = CreditNoteType.AccountsReceivable
+            };
+        }
+    }
+}
---
+++ CoreTests/Integration/Invoices/Deserialization.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xero.Api.Core.Model;
+using Xero.Api.Infrastructure.Interfaces;
+using Xero.Api.Serialization;
+using Xero.Api.Core.Model.Types;
+
+namespace CoreTests.Integration.Invoices
+{
+    [TestFixture]
+    public class Deserialization
+    {
+        private IJsonObjectMapper _jsonMapper;
+        private IXmlObjectMapper _xmlMapper;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _jsonMapper = new DefaultMapper();
+            _xmlMapper = new DefaultMapper();
+        }
+
+        [Test]
+        public void nullable_type_json()
+        {
+            var invoice = createInvoice();
+
+            var json = _jsonMapper.To(invoice);
+
+            var deserialized = _jsonMapper.From<Invoice>(json);
+
+            Assert.AreEqual(invoice.Id, deserialized.Id);
+            Assert.AreEqual(invoice.Type, deserialized.Type);
+        }
+
+        [Test]
+        public void nullable_type_xml()
+        {
+            var invoice = createInvoice();
+
+            var xml = _xmlMapper.To(invoice);
+
+            var deserialized = _xmlMapper.From<Invoice>(xml);
+
+            Assert.AreEqual(invoice.Id, deserialized.Id);
+            Assert.AreEqual(invoice.Type, deserialized.Type);
+        }
+
+        private static Invoice createInvoice()
+        {
+            return new Invoice()
+            {
+                Id = Guid.NewGuid(),
+                Type = InvoiceType.AccountsReceivable
+            };
+        }
+    }
+}
