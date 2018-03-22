0a6f69bd0a2e971c96a1814537d5294e597a9547
Matthew Steeples <matthew@mercuryit.co.uk>
2018-03-22 13:31:25 +0000

    Temporarily allow unknown invoice types

--- Xero.Api/Core/Model/Types/InvoiceType.cs
+++ Xero.Api/Core/Model/Types/InvoiceType.cs
@@ -5,6 +5,7 @@ namespace Xero.Api.Core.Model.Types
     [DataContract(Namespace = "")]
     public enum InvoiceType
     {
+        Unknown = 0,
         [EnumMember(Value = "ACCPAY")]
         AccountsPayable = 1,
         [EnumMember(Value = "ACCREC")]
