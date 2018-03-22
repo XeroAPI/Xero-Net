e02af4855ecdba19b7e0c093c6c3f1b9da26b53e
Matthew Steeples <matthew@mercuryit.co.uk>
2018-03-22 21:34:03 +0000

    Temporarily allow unknown CreditNote types

--- Xero.Api/Core/Model/Types/CreditNoteType.cs
+++ Xero.Api/Core/Model/Types/CreditNoteType.cs
@@ -5,9 +5,10 @@ namespace Xero.Api.Core.Model.Types
     [DataContract(Namespace = "")]
     public enum CreditNoteType
     {
+        Unknown = 0,
         [EnumMember(Value = "ACCPAYCREDIT")]
         AccountsPayable = 1,
         [EnumMember(Value = "ACCRECCREDIT")]
-        AccountsReceivable        
+        AccountsReceivable      
     }
 }
