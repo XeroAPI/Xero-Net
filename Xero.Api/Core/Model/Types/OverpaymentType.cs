8450a7e8a4dc00e0adc2427a880d7708f102c035
Matthew Steeples <matthew@mercuryit.co.uk>
2018-03-23 15:28:24 +0000

    Add 0 as valid values

--- Xero.Api/Core/Model/Types/OverpaymentType.cs
+++ Xero.Api/Core/Model/Types/OverpaymentType.cs
@@ -5,6 +5,7 @@ namespace Xero.Api.Core.Model.Types
     [DataContract(Namespace = "")]
     public enum OverpaymentType
     {
+        Unknown = 0,
         [EnumMember(Value = "SPEND-OVERPAYMENT")]
         SpendOverpayment = 1,
         [EnumMember(Value = "RECEIVE-OVERPAYMENT")]
--- Xero.Api/Core/Model/Types/PrepaymentType.cs
+++ Xero.Api/Core/Model/Types/PrepaymentType.cs
@@ -5,6 +5,7 @@ namespace Xero.Api.Core.Model.Types
     [DataContract(Namespace = "")]
     public enum PrepaymentType
     {
+        Unknown = 0,
         [EnumMember(Value = "SPEND-PREPAYMENT")]
         SpendPrepayment = 1,
         [EnumMember(Value = "RECEIVE-PREPAYMENT")]
