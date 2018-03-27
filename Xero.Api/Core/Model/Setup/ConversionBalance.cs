e25ff5e375f5942da482806d093b8c67f25dd129
Matthew Steeples <matthew@mercuryit.co.uk>
2018-03-27 16:03:56 +0100

    Add BalanceDetail (multi currency support) to Setup endpoint

--- Xero.Api/Core/Model/Setup/ConversionBalance.cs
+++ Xero.Api/Core/Model/Setup/ConversionBalance.cs
@@ -1,17 +1,34 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Xero.Api.Core.Model.Setup
 {
     [DataContract(Namespace = "")]
     public class ConversionBalance
     {
-        [DataMember]
-        public decimal Balance { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public decimal? Balance { get; set; }
 
         [DataMember]
         public string AccountCode { get; set; }
 
         [DataMember]
         public string Type { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public List<BalanceDetail> BalanceDetails { get; set; }
+    }
+
+    [DataContract(Namespace = "")]
+    public class BalanceDetail
+    {
+        [DataMember]
+        public decimal Balance { get; set; }
+
+        [DataMember]
+        public string CurrencyCode { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public decimal? CurrencyRate { get; set; }
     }
 }
