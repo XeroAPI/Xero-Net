12e081ee168b0999b44d15a25ee089416b3bc14e
Matthew Steeples <matthew@mercuryit.co.uk>
2019-01-13 13:09:51 +0000

    Add unknown to BankTransactionType
    Support case CX0008007682. The API returns an incorrect type (Journal SourceType) if the transaction fails to get created. If we want to log this message, we need a 0 value to be stored

--- Xero.Api/Core/Model/Types/BankTransactionType.cs
+++ Xero.Api/Core/Model/Types/BankTransactionType.cs
@@ -5,6 +5,7 @@ namespace Xero.Api.Core.Model.Types
     [DataContract(Namespace = "")]
     public enum BankTransactionType
     {
+        Unknown = 0,
         [EnumMember(Value = "RECEIVE")]
         Receive = 1,
         [EnumMember(Value = "SPEND")]
