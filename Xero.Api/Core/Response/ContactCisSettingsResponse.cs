5af3f7afbac7f5044d3b34eb116281d95edc6559
Miroslav Šalković <miroslav@ledgerscope.com>
2019-08-20 10:22:58 +0200

    CisSettings DataMember added to ContactCisSettingsResponse

--- Xero.Api/Core/Response/ContactCisSettingsResponse.cs
+++ Xero.Api/Core/Response/ContactCisSettingsResponse.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Xero.Api.Common;
 using Xero.Api.Core.Model;
 
 namespace Xero.Api.Core.Response
 {
+	[DataContract(Namespace = "")]
     public class ContactCisSettingsResponse : XeroResponse<ContactCisSetting>
     {
-        public List<ContactCisSetting> ContactCisSettings { get; set; }
+	    [DataMember(Name = "CISSettings")]
+		public List<ContactCisSetting> ContactCisSettings { get; set; }
 
         public override IList<ContactCisSetting> Values
         {
