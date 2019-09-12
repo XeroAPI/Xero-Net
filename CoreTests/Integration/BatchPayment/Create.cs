093c5737ea2e317886df67b24b6515da158052e4
Luke Kirby <Luke.Kirby@document-genetics.co.uk>
2019-09-12 11:15:40 +0100

    Changes to BatchPayment to avoid timezone Issue
    The payment date of the batch payment should be passed as a string in the format yyyy-MM-dd according to the Xero API docs page however as it was being parsed as a date, it was subject to the timezone difference between the API user and Xero, so if you parsed a date object without a timepart, you could end up with yesterday's date being set for the payment date.
    
    See https://developer.xero.com/documentation/api/batch-payments#PUT

---
+++ CoreTests/Integration/BatchPayment/Create.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Xero.Api.Core.Model;
+
+namespace CoreTests.Integration.BatchPayments
+{
+    [TestFixture]
+    public class Create : BatchPaymentsTest
+    {
+        [TestFixtureSetUp]
+        public void CreateBatchPaymentsSetUp()
+        {
+            SetUp();
+        }
+
+        [Test]
+        public void create_simple_batch_payment()
+        {
+            var date = DateTime.UtcNow;
+            const decimal expectedTotal = 32.6m;
+            const decimal invoiceAmount = 100;
+
+			var batchPayment = Given_a_batch_payment(invoiceAmount, date, expectedTotal);
+
+			Assert.IsNotNull(batchPayment);
+
+			Assert.AreEqual(expectedTotal, batchPayment.Total);
+			Assert.AreEqual(date.Date, batchPayment.Date);
+
+
+		}
+
+    }
+}
