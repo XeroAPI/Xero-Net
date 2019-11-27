86755c7ee233848129d8a5d2458e39645ed39d82
Matthew Steeples <matthew@ledgerscope.com>
2019-11-27 23:13:48 +0000

    Add Unknown for BankTransactionStatusEnum

--- Xero.Api/Core/Model/Status/BankTransactionStatus.cs
+++ Xero.Api/Core/Model/Status/BankTransactionStatus.cs
@@ -5,6 +5,7 @@ namespace Xero.Api.Core.Model.Status
     [DataContract(Namespace = "")]
     public enum BankTransactionStatus
     {
+        Unknown = 0,
         [EnumMember(Value = "AUTHORISED")]
         Authorised,
         [EnumMember(Value = "DELETED")]
